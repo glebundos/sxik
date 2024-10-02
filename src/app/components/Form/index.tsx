66f672bd2b02edb84d13d70da76269bf373901a2
lzy77_ <glebundos.me@gmail.com>
2024-10-02 16:25:00 +0300

    Update notes header.

--- src/app/components/Form/index.tsx
+++ src/app/components/Form/index.tsx
@@ -118,7 +118,7 @@ export default function NotesForm() {
       </form>
 
       <div className={s.notes}>
-        <h2>Сохраненные заметки</h2>
+        <h2>Сохранёнки</h2>
         {notes
           .slice()
           .reverse()
